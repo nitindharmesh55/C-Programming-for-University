624e4b502da011500f975f375ec885a67678c55c
Nitin <nitindharmesh01@gmail.com>
2026-07-14 20:34:07 +0530

    C Pyradmid Programm

--- CollegeC/main.c
+++ CollegeC/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
-void main()
+int main()
 {
 
     printf("My Name is Nitin\nmy Father Name is Babubhai\nmy mother name is jashodaben");
 
+    printf("\n");
+
+  for(int i = 0; i <= 5; i++)
+{
+    for(int j = 5; j > i; j--)
+    {
+        printf(" ");
+    }
+
+    for(int k = 1; k <= i + 1; k++)
+    {
+        printf("#");
+    }
+
+    for(int m = 0; m <= 1; m++)
+    {
+        printf(" ");
+    }
+
+    for(int n = 1; n <= i + 1; n++)
+    {
+        printf("#");
+    }
+
+    printf("\n");
+}
+
     // getc();
+    return 0;
 }
