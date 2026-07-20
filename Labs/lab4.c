bb3fcef0fa7e8294e5b0f0d7178c23783fb73ed8
Nitin <nitindharmesh01@gmail.com>
2026-07-20 21:33:34 +0530

    interchanging the code or value

---
+++ Labs/lab4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+// Write program interchange two numbers;
+
+int main(void)
+{
+    int a = 10, b= 20;
+    printf("Before swap A = %d, B = %d\n", a,b);
+
+    a  = a  + b;
+
+    b = a - b;
+
+    a = a - b;
+
+    printf("After swap: A = %d, B = %d\n", a , b);
+
+    return 0;
+
+}
